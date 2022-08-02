1b0a28a7d2e3882a88e68451f18c0b35651c761e
Peter Beard <peter@peterbeard.co>
2022-08-01 22:07:37 -0500

    Upgrade commander version

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import commander from 'commander';
+import {Command} from 'commander';
 import * as contact from './contact.js'
 import {Person} from './nomo.js';
 
@@ -18,8 +18,8 @@ const months = [
     'DEC',
 ];
 
-const program = commander.program;
-program.version('0.1.0');
+const program = new Command();
+program.version('0.5.0');
 program
     .option('-a, --address', 'Print the street address for each identity')
     .option('-b, --birthday', 'Print the birthday for each identity')
@@ -29,25 +29,26 @@ program
     .option('-c, --count <value>', 'How many identities to generate')
     .parse(process.argv);
 
-const count = program.count || 1;
+const options = program.opts();
+const count = options.count || 1;
 
 for (let i = 0; i < count; i++) {
     const person = Person.randomIdentity();
     console.log(`Name:      ${person.name}`);
-    if (program.birthday !== undefined) {
+    if (options.birthday !== undefined) {
         const year = person.birthday.getFullYear();
         const month = months[person.birthday.getMonth()];
         const day = person.birthday.getDate();
         console.log(`Birthday:  ${day} ${month} ${year} (age ${person.age})`)
     }
-    if (program.address !== undefined) {
+    if (options.address !== undefined) {
         const addressLines = person.address.toString().split('\n');
         console.log(`Address:   ${addressLines[0]}`);
         for (const line of addressLines.slice(1)) {
             console.log(`           ${line}`);
         }
     }
-    if (program.phone !== undefined) {
+    if (options.phone !== undefined) {
         const phone = contact.generatePhone();
         // Reformat the phone number from +1AAAEEELLLL to (AAA) EEE-LLLL
         const areaCode = phone.substr(2, 3);
@@ -55,11 +56,11 @@ for (let i = 0; i < count; i++) {
         const lineNumber = phone.substr(8, 4);
         console.log(`Phone:     (${areaCode}) ${exchangeCode}-${lineNumber}`);
     }
-    if (program.email !== undefined) {
+    if (options.email !== undefined) {
         const birthday = person.birthday;
         console.log(`Email:     ${contact.generateEmail(person.name, birthday)}`);
     }
-    if (program.job !== undefined) {
+    if (options.job !== undefined) {
         const job = person.job;
         console.log(`Job title: ${job}`);
     }
